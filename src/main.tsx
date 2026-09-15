c20df49b4a5511cea680c770c84ab44d9e7ddea1
xFactor <xfactorxai@gmail.com>
2026-09-14 20:05:09 -0500

    Add privacy-safe product analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import ErrorBoundary from './components/ErrorBoundary.tsx'
+import { startPlanetXAnalytics } from './services/planetxAnalytics'
+
+startPlanetXAnalytics()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
